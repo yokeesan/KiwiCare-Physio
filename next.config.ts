172de087466022281e7418e0f01b0d09e70424e1
yokeesan <yokeesanthanu@gmail.com>
2026-06-04 13:51:27 +0530

    fix: clean config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "standalone",
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
